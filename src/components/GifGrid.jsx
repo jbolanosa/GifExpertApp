218810138470a313f190eb0eb747f34db47af56d
José Bolaños <84158344+jbolanosa@users.noreply.github.com>
2025-04-11 23:00:25 -0600

    editando componentes

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,13 +1,27 @@
+import { useState, useEffect } from "react";
+import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({category}) => {
+    const [images, setimages] = useState([]);
 
+    const getImages = async() => {
+        const newImages = await getGifs(category);
+        setimages(newImages);
+    }
 
-
+    useEffect( () => {
+        getImages();
+    }, [ ])
 
     return (
         <>
             <h3>{category}</h3>
-            <p></p>
+            
+            <ol>
+                {images.map(({id, title}) => (
+                    <li key={id}>{title}</li>
+                ))}
+            </ol>
         </>
     )
 }
